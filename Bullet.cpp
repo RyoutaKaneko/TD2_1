76ef3a5a64543adeb0ccf35480ba23b48230ed05
kobashi_hayato <k021g1128@g.neec.ac.jp>
2022-10-06 14:50:33 +0900

    Vector3を追加

--- Bullet.cpp
+++ Bullet.cpp
@@ -16,12 +16,7 @@ void Bullet::Initialize(Model* model, const Vector3& position)
 
 void Bullet::Update()
 {
-	worldTransform_.matWorld_ = Identity();
-	worldTransform_.matWorld_ *= Scale(worldTransform_.scale_);
-	worldTransform_.matWorld_ *= RotX(worldTransform_.rotation_);
-	worldTransform_.matWorld_ *= RotY(worldTransform_.rotation_);
-	worldTransform_.matWorld_ *= RotZ(worldTransform_.rotation_);
-	worldTransform_.matWorld_ *= Trans(worldTransform_.translation_);
+
 	//çsóÒÇÃçƒåvéZ
 	worldTransform_.TransferMatrix();
 }
